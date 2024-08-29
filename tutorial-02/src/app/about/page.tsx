2da79ed25b75db7b1a41324d1cf539abbcea63a2
umairghani156 <umairghani156@gmail.com>
2024-08-29 15:55:37 +0500

    routing-Metadata

---
+++ tutorial-02/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Code with Mosh - About',
+}
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
--- tutorial-02/src/app/products/[productId]/page.tsx
+++ tutorial-02/src/app/products/[productId]/page.tsx
@@ -1,5 +1,20 @@
-export default function ProductId({params}:{
-    params: {productId: string}
-}) {
+import { Metadata } from "next";
+
+type Props = {
+    params:{
+        productId: string
+    }
+}
+export const generateMetadata =async ({params}: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Iphone ${params.productId}`)
+        }, 1000)
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+export default function ProductId({params}: Props) {
     return <h1 className="text-3xl">Details About product {params.productId}</h1>
 }
