69bfee0224c410c92430e69653b5b2610d459aa3
umairghani156 <umairghani156@gmail.com>
2024-08-30 20:45:14 +0500

    loading-UI

--- tutorial-02/src/app/blog/page.tsx
+++ tutorial-02/src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+export const metadata : Metadata = {
+    title:{
+        absolute: "Blog",
+    },
+}
 export default function MyBlog(){
     return <h1>Blog page</h1>
 }
