cfafe9d5d22a41dba0465e6bcb62afafa6b9c011
umairghani156 <umairghani156@gmail.com>
2024-09-06 07:39:56 +0500

    Handling-DELETE-request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -11,3 +11,42 @@ export async function GET(request: Request, {
     return Response.json(comment)
     
 }
+
+export async function PATCH(request: Request, {
+    params
+}: {
+    params: {
+        id: string
+    }
+}) {
+    const body = await request.json();
+    const {text} = body;
+    // const updatedComment = {
+    //     ...comments.find((comment) => comment.id === Number(params.id)),
+    //     ...comment
+    // }
+   // comments[comments.findIndex((comment) => comment.id === Number(params.id))] = updatedComment
+
+   const indexcomment = comments.findIndex((comment) => comment.id === Number(params.id));
+   comments[indexcomment].text = text;
+
+    return Response.json(comments[indexcomment])
+};
+
+export async function DELETE(request: Request, {
+    params
+}: {
+    params: {
+        id: string
+    }
+}) {
+
+   // const indexcomment = comments.splice(comments.findIndex((comment) => comment.id === Number(params.id)), 1);
+   const index = comments.findIndex((comment) => comment.id === Number(params.id));
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json({
+        status: 200,
+        ...deletedComment
+    })
+}
