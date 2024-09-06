c572082282a07f17992e187455e204a800211a1a
umairghani156 <umairghani156@gmail.com>
2024-09-06 08:40:47 +0500

    Cookies-in-Route-handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,15 +1,20 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest){
     const requestHeaders = new Headers(request.headers);
     const headerList = headers();
+    cookies().set("getResultPage","2");
+    const theme = request.cookies.get("theme");
     console.log(requestHeaders.get("Authorization"));
     console.log(headerList.get("Authorization"));
+    console.log(theme);
+    console.log(cookies().get("getResultPage"));
     
     return new Response("<h1>API Profile</h1>",{
         headers:{
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     })
 }
