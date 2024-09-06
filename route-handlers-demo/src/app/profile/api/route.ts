415843f9da9132ef6ad87c4849620707e9393881
umairghani156 <umairghani156@gmail.com>
2024-09-06 08:15:55 +0500

    Headers-in-Routes-Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET(){
-    return new Response("API Profile")
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+    
+    return new Response("<h1>API Profile</h1>",{
+        headers:{
+            "Content-Type": "text/html"
+        }
+    })
 }
