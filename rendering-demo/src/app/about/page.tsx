4bfe18ac0b9afae66bc4e19de8936cc3130ee5bd
umairghani156 <umairghani156@gmail.com>
2024-09-11 14:52:40 +0500

    dynamic-rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers";
 
 const AboutPage = () => {
+  const cookiesStore = cookies();
+  const theme = cookiesStore.get("theme");
+  console.log(theme);
+  
    
     console.log("About Page");
     
