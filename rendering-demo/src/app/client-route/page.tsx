1eb5ff7991811da66b980d567775a4e2ee3d0513
umairghani156 <umairghani156@gmail.com>
2024-09-22 14:07:58 +0500

    addedGitignore

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -13,6 +13,8 @@ export default function ClientRoutePage() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
       };
       return (
         <>
